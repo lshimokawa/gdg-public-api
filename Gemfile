fd43392835395b32da771dde201e660eec83bba3
Lennon Shimokawa <lshimokawa@gmail.com>
2013-02-22 19:14:14 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+gem 'sinatra'
+gem 'sinatra'
+gem 'json'
+gem 'nokogiri'
+gem 'open-uri'
---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'json'
+require 'nokogiri'
+require 'open-uri'
+
+get '/events/:id' do
+  content_type :json
+  url = "https://developers.google.com/events/#{params[:id]}/"
+  puts url
+  data = Nokogiri::HTML(open(url))
+
+  map = Hash.new
+  data.css("header").each do |item|  
+    key = item.text.strip.delete(":").downcase.gsub(/\s+/, "_")
+    value = item.next.text.strip  
+    map[key] = value
+  end
+
+  map.to_json
+end
+
+
+
